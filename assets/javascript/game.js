b2c2eb793b504fdffe01e637c0df8354d65bdde6
Richard Lieu <CeciliaWang@Cecilia-Ximeng-Wangs-MacBook-Pro.local>
2017-08-30 13:59:36 -0700

    update game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,89 +1,111 @@
     //Creates array for game words.
-    var gameWords = ["coinbase", "ethereum", "litecoin"];
-    
+    var gameWords = ["bitcoin", "ethereum", "litecoin"];
     //Selects elements to display stats.
     var user = document.getElementById("user");
     var wins = document.getElementById("wins");
     var losses = document.getElementById("losses");
     var guesses = document.getElementById("guesses");
     var word = document.getElementById("word");
-    var status = document.getElementById("status");
     //Tracks guesses, wins and losses
     var winCount = 0;
     var lossCount = 0;
-    var round = 0;
     var guessCount = 15;
+    //Tracks round number and sets current word.
+    var round = 0;
+    var currentWord = gameWords[round];
     // Creates an array that lists guessed letters.
     var userGuessed = [];
-    var correctLetters = ["_","_","_","_","_","_","_","_"];
-    word.textContent = "_ _ _ _ _ _ _ _";
+    //Sets number of blanks based on current word.
+    var correctLetters = [];
+    for (var i = 0; i < currentWord.length; i++) {
+
+        correctLetters.push("_");
+    
+    }
+    word.textContent = correctLetters.join(" ");
 
     // This function is run whenever the user presses a key.
     document.onkeyup = function(event) {
 
-        if (round < 3) {
-
-            //Sets current word based on round number.
-            var currentWord = gameWords[round];
+        if (round < gameWords.length) {
 
             if (guessCount > 0) {
                 
                 // Determines which key was pressed and adds to array.
                 var userGuess = event.key;
                     
-                // for (var i = 0; i < userGuessed.length; i++) {
-                //     if (userGuess == userGuessed[i]) {
+                if (userGuessed.includes(userGuess)) {
 
-                //         alert("You've already guessed this letter!");
-                //     } else {
+                    alert("You've already guessed this letter!");
+
+                } else {
                     
-                        userGuessed.push(userGuess);
-                        user.textContent = userGuessed;
-                        //Checks user's guess against letters in current word.
-                        for (var i = 0; i < currentWord.length; i++) {
-                            if (userGuess == currentWord[i]) {
-                                correctLetters[i] = currentWord[i];
-                                var displayLetters = correctLetters[0] + " " + correctLetters[1] + " " + correctLetters[2] + " " + correctLetters[3] + " " + correctLetters[4] + " " + correctLetters[5] + " " + correctLetters[6] + " " + correctLetters[7];
-                                word.textContent = displayLetters;
-                            }
+                    userGuessed.push(userGuess);
+                    user.textContent = userGuessed;
+                    //Checks user's guess against letters in current word.
+                    for (var i = 0; i < currentWord.length; i++) {
+                        if (userGuess == currentWord[i]) {
+                            correctLetters[i] = currentWord[i];
+                            //var displayLetters = correctLetters[0] + " " + correctLetters[1] + " " + correctLetters[2] + " " + correctLetters[3] + " " + correctLetters[4] + " " + correctLetters[5] + " " + correctLetters[6] + " " + correctLetters[7];
+                            
+                            word.textContent = correctLetters.join(" ");
                         }
-                        guessCount--
-                        guesses.textContent = guessCount;
-                    //}
-                //}
+                    }
+                    guessCount--
+                    guesses.textContent = guessCount;
+                    
+                }
             }
             //Checks if all letters in current word have been guessed (win). 
-            if (correctLetters[0] + correctLetters[1] + correctLetters[2] + correctLetters[3] + correctLetters[4] + correctLetters[5] + correctLetters[6] + correctLetters[7] == currentWord) {
+            if (correctLetters.join("") == currentWord) {
                 winCount++
                 wins.textContent = winCount;
                 round++
-                
-                correctLetters = ["_","_","_","_","_","_","_","_"];
-                userGuessed = [];
-                user.textContent = userGuessed;
-                word.textContent = "_ _ _ _ _ _ _ _";
-                // for ( var i = 0; i< currentWord.length; i++) {
-                //     correctLetters.push("_");
-                // }
-                guessCount = 15;
-                guesses.textContent = guessCount;
+                if (round < gameWords.length) {
+                    //Sets current word based on round number.
+                    currentWord = gameWords[round];
+                    //Reset round.
+                    userGuessed = [];
+                    user.textContent = userGuessed;
+                    guessCount = 15;
+                    guesses.textContent = guessCount;
+                    correctLetters = [];
+                    //Sets number of blanks based on current word.
+                    for (var i = 0; i < currentWord.length; i++) {
+
+                        correctLetters.push("_");
+                    
+                    }
+                    word.textContent = correctLetters.join(" ");
+                } else {
+                    document.getElementById("status").innerHTML = "Game Over";
+                }
+
             } else if (guessCount == 0) {
                 lossCount++
                 losses.textContent = lossCount;
                 round++
-                
-                correctLetters = ["_","_","_","_","_","_","_","_"];
-                userGuessed = [];
-                user.textContent = userGuessed;
-                word.textContent = "_ _ _ _ _ _ _ _";
-                // for ( var i = 0; i< currentWord.length; i++) {
-                //     correctLetters.push("_");
-                // }
-                guessCount = 15;
-                guesses.textContent = guessCount;
+                if (round < gameWords.length) {
+                    //Sets current word based on round number.
+                    currentWord = gameWords[round];
+                    //Reset round.
+                    userGuessed = [];
+                    user.textContent = userGuessed;
+                    guessCount = 15;
+                    guesses.textContent = guessCount;
+                    correctLetters = [];
+                    //Sets number of blanks based on current word.
+                    for (var i = 0; i < currentWord.length; i++) {
+
+                        correctLetters.push("_");
+                    
+                    }
+                    word.textContent = correctLetters.join(" ");
+                } else {
+                    document.getElementById("status").innerHTML = "Game Over";
+                }
             }
         
-        } else {
-            status.textContent = "Game Over";
-        }
+        } 
+
     };
